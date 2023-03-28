270437273956e17e6ff66371c30b0aabd1d222a5
bmchun00 <bmchun00@korea.ac.kr>
2023-03-28 22:27:29 +0900

    bottombar

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,19 +1,58 @@
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:playground/myPage.dart';
 import 'package:playground/splash.dart';
 
+Widget PageBuilder(int index){
+  switch(index){
+    case 0:
+      return Scaffold(
+        body: Text("This is Main Page"),
+      );
+    case 1:
+      return Scaffold(
+        body: Text("This is My Page 1"),
+      );
+    case 2:
+      return Scaffold(
+        body: Text("This is My Page 2"),
+      );
+    case 3:
+      return Scaffold(
+        body: Text("This is My Page 3"),
+      );
+    case 4:
+      return MyPage();
+    default:
+      return Scaffold(
+        body: Text("This is Error Page"),
+      );
+  }
+}
 
 class MainPage extends StatefulWidget{
   @override
   State<MainPage> createState() => _MainPage();
 }
 
-class _MainPage extends State<MainPage>{
+class _MainPage extends State<MainPage> with TickerProviderStateMixin{
+  TabController? controller;
   int _currentIndex = 0;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose(){
+    controller!.dispose();
+    super.dispose();
+  }
 
   Widget _buildFloatingBar() {
     return CustomNavigationBar(
-      
       blurEffect: true,
       iconSize: 30.0,
       selectedColor: mColor1,
@@ -64,6 +103,7 @@ class _MainPage extends State<MainPage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      body: PageBuilder(_currentIndex),
       bottomNavigationBar: _buildFloatingBar(),
     );
   }
---
+++ lib/myPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget MyPage(){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("My Page.."),
+    ),
+  );
+}
