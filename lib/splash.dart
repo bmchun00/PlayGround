2220bce14e5243a481c4bf287eb4ffa0ba8b2ab7
bmchun00 <bmchun00@korea.ac.kr>
2023-03-29 12:34:52 +0900

    splash animation

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:page_transition/page_transition.dart';
 
 import 'login.dart';
 
@@ -13,17 +14,52 @@ class SplashScreen extends StatefulWidget{
   State<SplashScreen> createState() => _SplashScreen();
 }
 
+class CustomPageRoute<T> extends PageRoute<T> {
+  CustomPageRoute(this.child);
+  @override
+  // TODO: implement barrierColor
+  Color get barrierColor => Colors.transparent;
+
+  @override
+  String? get barrierLabel => null;
+
+  final Widget child;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 500);
+}
+
 class _SplashScreen extends State<SplashScreen>{
-  String? title;
+
+
+  String? title1;
+  String? title2;
+  Widget? titleWidget1;
+  Widget? titleWidget2;
 
   @override
   void initState(){
     super.initState();
     loadData();
-    title = "";
+    title1 = "";
+    title2 = "";
+
     Future.delayed(const Duration(milliseconds: 2000), () {
       setState(() {
-        title = "Ground.com";
+        titleWidget1 = getCustomAnimatedText("lay", mColor1,300);
+        titleWidget2 = getCustomAnimatedText("round.com", mColor2,100);
       });
     });
   }
@@ -32,8 +68,25 @@ class _SplashScreen extends State<SplashScreen>{
     return Timer(Duration(seconds: 5),onDoneLoading);
   }
 
-  onDoneLoading() async { //자동 로그인이 설정되어있는 경우도 있음
-    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+  onDoneLoading(){ //자동 로그인이 설정되어있는 경우도 있음
+    Navigator.of(context).pushReplacement(CustomPageRoute(LoginPage()));
+  }
+
+  DefaultTextStyle getCustomAnimatedText(String text, Color color, int spd){
+    return DefaultTextStyle(
+      style: TextStyle(
+        fontSize: 40.0,
+        fontFamily: 'Pacifico',
+        color: color,
+      ),
+      child: AnimatedTextKit(
+        animatedTexts: [
+          TyperAnimatedText(text, speed: Duration(milliseconds: spd)),
+        ],
+        repeatForever: false,
+        totalRepeatCount: 1,
+      ),
+    );
   }
 
   @override
@@ -45,22 +98,10 @@ class _SplashScreen extends State<SplashScreen>{
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Play",style: TextStyle(fontFamily: 'Pacifico',fontSize: 40,color: mColor1),),
-                DefaultTextStyle(
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      fontFamily: 'Pacifico',
-                      color: mColor2,
-                    ),
-                    child: AnimatedTextKit(
-                      animatedTexts: [
-                        TyperAnimatedText(title!, speed: Duration(milliseconds: 70)),
-                      ],
-                      onTap: () {
-                      },
-                      isRepeatingAnimation: true,
-                    ),
-                ),
+                Text("P",style: TextStyle(fontFamily: 'Pacifico',fontSize: 40,color: mColor1),),
+                titleWidget1 ?? Text(""),
+                Text("G",style: TextStyle(fontFamily: 'Pacifico',fontSize: 40,color: mColor2),),
+                titleWidget2 ?? Text(""),
               ],
             )
           )
